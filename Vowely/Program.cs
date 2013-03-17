136727888f9a0b3dc34552940d5f9ffbbcc712ef
Mark Wetzel <mark@markwetzel.com>
2013-03-17 13:42:59 -0700

    Initial commit.

---
+++ Vowely/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Extensions;
+
+namespace Vowely
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to count the vowels!: ");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> vowels = new Dictionary<char, int>();
+
+            int vowelCount = 0;
+            foreach (char c in input) {
+                if (c.IsVowel()) {
+                    vowelCount++;
+                    if (!vowels.ContainsKey(c)) {
+                        vowels[c] = 0;
+                    }
+                    vowels[c]++;
+                }
+            }
+
+            Console.WriteLine("\nThere are {0} vowels in the string {1}! Isn't that just dandy?", vowelCount, input);
+
+            Console.WriteLine("\nA little more info...\n");
+
+            foreach (KeyValuePair<char, int> item in vowels) {
+                Console.WriteLine("{0} occurs {1} times", item.Key, item.Value);
+            }
+
+            Console.WriteLine("\nThe more you know...");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vowely/lib/MyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class MyExtensions
+    {
+        public static bool IsVowel(this char c)
+        {
+            c = char.ToLower(c);
+
+            switch (c) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
